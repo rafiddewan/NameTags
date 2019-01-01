78e83937ad9256f06e2a1a20052cf39f327b52db
rafiddewan <rafid.dewan145@gmail.com>
2018-12-31 21:58:06 -0500

    Made a test name tag

--- NameTagGenerator.py
+++ NameTagGenerator.py
@@ -1,3 +1,9 @@
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageFont
 img = Image.open('School-Name-Tag.jpg')
+draw = ImageDraw.Draw(img)
+font = ImageFont.truetype("/usr/share/fonts/truetype/freefont/FreeMono.ttf", 28, encoding="unic")
+draw.text((100, 60),"Rafid Dewan",(0,0,0),font=font)
+draw.text((40,90), "Carleton University", (0,0,0), font =font)
+font = ImageFont.truetype("/usr/share/fonts/truetype/freefont/FreeMono.ttf", 25, encoding="unic")
+draw.text((40,125), "Software  Engineering", (0,0,0), font =font)
 img.show();
