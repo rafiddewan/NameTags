dd32db4bee3572dc26cca0e3c0b79e4f8e7a23df
rafiddewan <rafid.dewan145@gmail.com>
2019-01-01 03:23:11 -0500

    Made a ID number for every attendee in case if two people are from the same institution

--- NameTagGenerator.py
+++ NameTagGenerator.py
@@ -1,7 +1,8 @@
 from PIL import Image, ImageDraw, ImageFont
 import csv
+import random
 
-def createNameTag(title, colour, first_name, last_name, institution,occupation, ID):
+def createNameTag(title, colour, first_name, last_name, institution,occupation, id):
   img = Image.open(title + '.jpg')
   draw = ImageDraw.Draw(img)
   font = ImageFont.truetype("/usr/share/fonts/truetype/freefont/FreeMono.ttf", 28, encoding="unic")
@@ -9,9 +10,25 @@ def createNameTag(title, colour, first_name, last_name, institution,occupation, 
   draw.text((40,90), institution, colour, font = font)
   font = ImageFont.truetype("/usr/share/fonts/truetype/freefont/FreeMono.ttf", 25, encoding ="unic")
   draw.text((40,125), occupation, colour, font = font)
-  filename = first_name + "_" + last_name + "_" + institution + "_" + occupation + "_" + str(ID)
+  filename = first_name + "_" + last_name + "_" + institution + "_" + str(id)
   img.save('Test/{}.jpg'.format(filename))
   return
+
+def idMaker():
+  id = random.randint(1,351)
+  with open('id.csv', newline='') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    for row in csv_reader:
+      if(line_count == 0 and row == ''):
+        break;
+      elif row == str(id):   
+        idMaker()
+  with open('id.csv', mode='w') as id_file:
+    id_writer = csv.writer(id_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    id_writer.writerow(str(id))
+  return id
+
 
 title = 'Organizer'
 
@@ -21,11 +38,10 @@ if((title != 'Organizer') and (title != 'Attendee') and (title != 'Volunteer')):
 
 with open(title + ".csv") as csv_file:
   csv_reader = csv.reader(csv_file, delimiter=',')
-  ID = 0;
   for row in csv_reader:
     first_name = row[0]
     last_name = row[1]
     institution = row[2]
     occupation = row[3]
-    ID += 1
-    createNameTag(title, colour, first_name, last_name, institution, occupation, ID)
+    id = idMaker();
+    createNameTag(title, colour, first_name, last_name, institution, occupation, id)
