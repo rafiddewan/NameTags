f731da420ed0fe6477149ca8fa6954fa97e08d2c
rafiddewan <rafid.dewan145@gmail.com>
2019-01-01 01:53:54 -0500

    implemented a unique ID to each  name tag

--- NameTagGenerator.py
+++ NameTagGenerator.py
@@ -1,7 +1,7 @@
 from PIL import Image, ImageDraw, ImageFont
 import csv
 
-def createNameTag(title, colour, first_name, last_name, institution,occupation):
+def createNameTag(title, colour, first_name, last_name, institution,occupation, ID):
   img = Image.open(title + '.jpg')
   draw = ImageDraw.Draw(img)
   font = ImageFont.truetype("/usr/share/fonts/truetype/freefont/FreeMono.ttf", 28, encoding="unic")
@@ -9,7 +9,8 @@ def createNameTag(title, colour, first_name, last_name, institution,occupation):
   draw.text((40,90), institution, colour, font = font)
   font = ImageFont.truetype("/usr/share/fonts/truetype/freefont/FreeMono.ttf", 25, encoding ="unic")
   draw.text((40,125), occupation, colour, font = font)
-  img.save('Test/{}.jpg'.format(first_name + "_" + last_name + "_" + institution + "_" + occupation))
+  filename = first_name + "_" + last_name + "_" + institution + "_" + occupation + "_" + str(ID)
+  img.save('Test/{}.jpg'.format(filename))
   return
 
 title = 'Organizer'
@@ -20,9 +21,11 @@ if((title != 'Organizer') and (title != 'Attendee') and (title != 'Volunteer')):
 
 with open(title + ".csv") as csv_file:
   csv_reader = csv.reader(csv_file, delimiter=',')
+  ID = 0;
   for row in csv_reader:
     first_name = row[0]
     last_name = row[1]
     institution = row[2]
     occupation = row[3]
-    createNameTag(title, colour, first_name, last_name, institution, occupation)
+    ID += 1
+    createNameTag(title, colour, first_name, last_name, institution, occupation, ID)
