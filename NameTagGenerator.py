b2e0ca7397695ef0f5270f2de063845606674c8d
rafiddewan <rafid.dewan145@gmail.com>
2018-12-31 22:18:13 -0500

    Added variables so that you can manipulate the name, program and university

--- NameTagGenerator.py
+++ NameTagGenerator.py
@@ -1,10 +1,14 @@
 from PIL import Image, ImageDraw, ImageFont
+first_name = "Rafid"
+last_name = "Dewan"
+university = "Carleton Univeristy"
+program = "Software Engineering"
 img = Image.open('School-Name-Tag.jpg')
 draw = ImageDraw.Draw(img)
 font = ImageFont.truetype("/usr/share/fonts/truetype/freefont/FreeMono.ttf", 28, encoding="unic")
-draw.text((100, 60),"Rafid Dewan",(0,0,0),font=font)
-draw.text((40,90), "Carleton University", (0,0,0), font =font)
-font = ImageFont.truetype("/usr/share/fonts/truetype/freefont/FreeMono.ttf", 25, encoding="unic")
-draw.text((40,125), "Software  Engineering", (0,0,0), font =font)
+draw.text((100, 60),first_name + " " + last_name,(0,0,0),font = font)
+draw.text((40,90), university, (0,0,0), font = font)
+font = ImageFont.truetype("/usr/share/fonts/truetype/freefont/FreeMono.ttf", 25, encoding ="unic")
+draw.text((40,125), program, (0,0,0), font = font)
 img.show()
-img.save('Test/{}.jpg'.format("Rafid_Dewan"))
+img.save('Test/{}.jpg'.format(first_name + "_" + last_name + "_" + university + "_" + program))
