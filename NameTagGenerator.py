0f811f988d40899e0080380fbb5694c4eccf0398
rafiddewan <rafid.dewan145@gmail.com>
2018-12-31 22:10:01 -0500

    Can now save nametags in a folder

--- NameTagGenerator.py
+++ NameTagGenerator.py
@@ -6,4 +6,5 @@ draw.text((100, 60),"Rafid Dewan",(0,0,0),font=font)
 draw.text((40,90), "Carleton University", (0,0,0), font =font)
 font = ImageFont.truetype("/usr/share/fonts/truetype/freefont/FreeMono.ttf", 25, encoding="unic")
 draw.text((40,125), "Software  Engineering", (0,0,0), font =font)
-img.show();
+img.show()
+img.save('Test/{}.jpg'.format("Rafid_Dewan"))
